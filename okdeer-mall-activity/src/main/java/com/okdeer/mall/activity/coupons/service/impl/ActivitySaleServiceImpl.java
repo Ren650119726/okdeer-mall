cc39f91351191b9343e8712a11400bcc9ace114c
zengj <zengj@yschome.com>
2016-09-12 18:04:43 +0800

    去掉测试代码 去掉测试代码

--- okdeer-mall-activity/src/main/java/com/okdeer/mall/activity/coupons/service/impl/ActivitySaleServiceImpl.java
+++ okdeer-mall-activity/src/main/java/com/okdeer/mall/activity/coupons/service/impl/ActivitySaleServiceImpl.java
@@ -4,7 +4,6 @@ package com.okdeer.mall.activity.coupons.service.impl;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -538,21 +537,5 @@ public class ActivitySaleServiceImpl implements ActivitySaleServiceApi, Activity
 	@Override
 	public int selectActivitySale(String activityId) {
 		return activitySaleMapper.selectActivitySale(activityId);
-	}
-
-	public static void main(String[] args) {
-		List<String> list = new ArrayList<String>();
-		list.add("保护环境"); // 向列表中添加数据
-		list.add("爱护地球"); // 向列表中添加数据
-		list.add("从我做起"); // 向列表中添加数据
-		List<String> list1 = new ArrayList<String>();
-		list1.add("保护环境"); // 向列表中添加数据
-		list1.add("爱护地球"); // 向列表中添加数据
-		list1.add("爱护地球ddd"); // 向列表中添加数据
-		boolean ret = list.removeAll(list1); // 从list中移除与list1相同的元素
-		Iterator<String> it = list.iterator(); // 创建迭代器
-		while (it.hasNext()) { // 循环遍历迭代器
-			System.out.println(it.next()); // 输出集合中元素
-		}
 	}
 }
