f1f27b1511942a6994b9cb632b07c82c358461e0
tuzhd <tuzhd@yschome.com>
2016-09-28 17:13:35 +0800

    切换新的读取zookeeper及redis配置方式

--- okdeer-mall-service/src/main/java/com/okdeer/mall/config/RedisTemplateConfig.java
+++ okdeer-mall-service/src/main/java/com/okdeer/mall/config/RedisTemplateConfig.java
@@ -1,8 +1,6 @@
 
 package com.okdeer.mall.config;
 
-import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
