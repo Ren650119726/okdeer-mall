db382f71da67c47a167799bf6cd7b338ae55d4fd
zhaoqc <zhaoqc@yschome.com>
2016-09-30 13:23:02 +0800

    新增用户填写推荐人邀请码

--- okdeer-mall-system/src/main/java/com/okdeer/mall/system/mapper/SysUserInvitationCodeMapper.java
+++ okdeer-mall-system/src/main/java/com/okdeer/mall/system/mapper/SysUserInvitationCodeMapper.java
@@ -2,6 +2,8 @@ package com.okdeer.mall.system.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.okdeer.mall.system.entity.SysUserInvitationCode;
 import com.okdeer.mall.system.entity.SysUserInvitationCodeVo;
 import com.okdeer.base.dal.IBaseCrudMapper;
@@ -16,9 +18,30 @@ import com.okdeer.base.dal.IBaseCrudMapper;
  *     Task ID			  Date			     Author		      Description
  * ----------------+----------------+-------------------+-------------------------------------------
  *		重构4.1			 2016年9月19日 			zhulq
+ *      V1.1.0           2016年9月28日                                zhaoqc        添加根据用户Id查询邀请码信息 
  */
 public interface SysUserInvitationCodeMapper extends IBaseCrudMapper {
 
+	/**
+	 * 
+	 * @Description: 根据用户ID查询用户邀请码信息 </p>
+	 * @param sysBuyerUserId
+	 * @return
+	 * @author yangq
+	 * @date 2016年9月28日
+	 */
+	SysUserInvitationCode selectInvitationById(String sysBuyerUserId);
+	
+	/**
+	 * 
+	 * @Description: 根据邀请码code查询用户邀请码信息 </p>
+	 * @param code
+	 * @return
+	 * @author yangq
+	 * @date 2016年9月28日
+	 */
+	SysUserInvitationCode selectInvitationByCode(String InvitationCode);
+	
 	/**
 	 * @Description: 获取邀请码列表
 	 * @param sysUserInvitationCodeVo 邀请码vo
@@ -43,4 +66,19 @@ public interface SysUserInvitationCodeMapper extends IBaseCrudMapper {
 	 * @date 2016年9月26日
 	 */
 	void updateCode(SysUserInvitationCode sysUserInvitationCode);
+
+	/**
+     * 根据用户Id查询邀请码信息
+     * @param userId 用户Id
+     * @param userType 用户类型
+     * @return 邀请码实体
+     */
+    SysUserInvitationCode findInvitationCodeByUserId(@Param("userId") String userId, @Param("userType")int userType);
+    
+    /**
+     * 根据邀请码查找邀请码实体
+     * @param invitationCode 邀请码
+     * @return 邀请码实体
+     */
+    SysUserInvitationCode findInvitationCodeByCode(@Param("invitationCode") String invitationCode);
 }
