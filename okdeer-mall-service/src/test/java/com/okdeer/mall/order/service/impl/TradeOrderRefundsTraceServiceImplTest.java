9e44c4e20ee9c5a8ca4b8ecf34d443c21104b61d
maojj <maojj@YSC-MAOJJ.kingser.com>
2016-10-12 13:14:46 +0800

    修改测试用例

--- okdeer-mall-service/src/test/java/com/okdeer/mall/order/service/impl/TradeOrderRefundsTraceServiceImplTest.java
+++ okdeer-mall-service/src/test/java/com/okdeer/mall/order/service/impl/TradeOrderRefundsTraceServiceImplTest.java
@@ -49,7 +49,7 @@ public class TradeOrderRefundsTraceServiceImplTest {
 		refundsOrder.setId("8a98681b56115c7401561169e9ba000c");
 		refundsOrder.setType(OrderTypeEnum.PHYSICAL_ORDER);
 		refundsOrder.setRefundNo("XT16072215322751");
-		tradeOrderRefundsTraceService.saveRefundApplyTrace(refundsOrder);
+		tradeOrderRefundsTraceService.saveRefundTrace(refundsOrder);
 	}
 
 	@Test
@@ -58,13 +58,17 @@ public class TradeOrderRefundsTraceServiceImplTest {
 		TradeOrderRefunds refundsOrder = new TradeOrderRefunds();
 		refundsOrder.setId("8a98681b56115c7401561169e9ba000c");
 		refundsOrder.setRefundsStatus(RefundsStatusEnum.WAIT_BUYER_RETURN_GOODS);
-		tradeOrderRefundsTraceService.saveSellerDealTrace(refundsOrder);
+		tradeOrderRefundsTraceService.saveRefundTrace(refundsOrder);
 	}
 
 	
 	@Test
 	public void testSaveBuyerReturnTrace() {
-		tradeOrderRefundsTraceService.saveBuyerReturnTrace("8a98681b56115c7401561169e9ba000c", RefundsLogisticsEnum.DOOR_PICK_UP);
+		TradeOrderRefunds refundsOrder = new TradeOrderRefunds();
+		refundsOrder.setId("8a98681b56115c7401561169e9ba000c");
+		refundsOrder.setRefundsStatus(RefundsStatusEnum.WAIT_SELLER_REFUND);
+		refundsOrder.setLogisticsType(RefundsLogisticsEnum.DOOR_PICK_UP);
+		tradeOrderRefundsTraceService.saveRefundTrace(refundsOrder);
 	}
 	
 	@Test
@@ -73,7 +77,7 @@ public class TradeOrderRefundsTraceServiceImplTest {
 		TradeOrderRefunds refundsOrder = new TradeOrderRefunds();
 		refundsOrder.setId("8a98681b56115c7401561169e9ba000c");
 		refundsOrder.setRefundsStatus(RefundsStatusEnum.REFUND_SUCCESS);
-		tradeOrderRefundsTraceService.saveSellerRefundTrace(refundsOrder);
+		tradeOrderRefundsTraceService.saveRefundTrace(refundsOrder);
 	}
 	
 	@Test
