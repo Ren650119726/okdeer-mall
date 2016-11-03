c4fc80e0cc2f262ee5b2f27c3db0a648c2a600ea
yangq <yangq@yml-yangq.kingser.com>
2016-11-03 10:49:11 +0800

    单元测试代码

---
+++ okdeer-mall-service/src/test/java/com/okdeer/mall/order/service/impl/TradeOrderProcessServiceImplTest.java
@@ -0,0 +1,79 @@
+/** 
+ *@Project: okdeer-mall-service 
+ *@Author: yangq
+ *@Date: 2016年10月31日 
+ *@Copyright: ©2014-2020 www.okdeer.com Inc. All rights reserved. 
+ */
+
+package com.okdeer.mall.order.service.impl;
+
+import javax.annotation.Resource;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.okdeer.mall.base.BaseTest;
+import com.okdeer.mall.order.service.TradeOrderProcessServiceApi;
+import com.okdeer.mall.order.vo.TradeOrderRespDto;
+
+/**
+ * ClassName: TradeOrderProcessServiceImplTest 
+ * @Description: TODO
+ * @author yangq
+ * @date 2016年10月31日
+ *
+ * =================================================================================================
+ *     Task ID			  Date			     Author		      Description
+ * ----------------+----------------+-------------------+-------------------------------------------
+ *
+ */
+public class TradeOrderProcessServiceImplTest extends BaseTest{
+
+	@Resource
+	private TradeOrderProcessServiceApi tradeOrderProcessServiceApi;
+	
+	/**
+	 * 
+	 * @Description: 结算单元测试 </p>
+	 * @author yangq
+	 * @date 2016年10月31日
+	 */
+	@Test
+	public void testValidateStoreSkuStock() {
+
+		// 结算入参
+		String requestStr = "{\"data\": {\"storeId\": \"5646258e276511e6aaff00163e010eb1\",\"startMoney\": \"1.00\",\"list\": [{\"skuNum\": \"1\",\"skuPrice\": \"0.01\",\"skuId\": \"001f3c6e276511e6aaff00163e010eb1\",\"updateTime\": \"2016-10-07 19:08:03\",\"isPrivilege\": \"0\"}],\"userId\": \"14527626891242d4d00a207c4d69bd80\",\"fare\": \"5.00\"}}";
+		try {
+			TradeOrderRespDto respDto = tradeOrderProcessServiceApi.validateStoreSkuStock(requestStr);
+			//Assert.assertNull("should be null", respDto); // 查看对象是否为空
+			Assert.assertNotNull("should not be null", respDto); // 查看对象是否不为空
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * 
+	 * @Description: 确认订单单元测试</p>
+	 * @author yangq
+	 * @date 2016年10月31日
+	 */
+	@Test
+	public void testAddTradeOrder() {
+
+		String comfirmStr = "{\"data\": { \"recordId\": \"\",\"activityItemId\": \"\", \"storeName\":\"国际公馆一、二期快送店\",\"remark\":\"\",\"pickTime\":\"\",\"fare\":\"0.00\",\"list\":[{\"skuId\":\"001f3c6e276511e6aaff00163e010eb1\",\"skuNum\":\"1\",\"skuPrice\":\"440\",\"updateTime\":\"2015-12-25 08:44:32\",\"isPrivilege\": \"0\"}],\"couponsType\":\"\",\"type\":\"0\",\"orderResource\":\"0\",\"invoiceHead\":\"\",\"addressId\":\"000253b3276311e6aaff00163e010eb1\",\"activityId\":\"\",\"isInvoice\":\"0\",\"userId\":\"00a3c1a8a1b949e5b5e27efc6a377525\",\"receiveTime\": \"\",\"payType\": \"1\",\"userPhone\":\"13925296062\",\"activityType\":\"0\",\"storeId\":\"5646258e276511e6aaff00163e010eb1\",\"pickType\":\"0\",\"invoiceContent\":\"\"}}";
+
+		try {
+			TradeOrderRespDto respDto = tradeOrderProcessServiceApi.addTradeOrder(comfirmStr);
+			Assert.assertNotNull("should not be null", respDto); // 查看对象是否不为空
+			Assert.assertNull("should be null", respDto); // 查看对象是否为空
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	
+	
+}
