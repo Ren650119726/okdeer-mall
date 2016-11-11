637a29195ea2779d48a4a18f24e95ab61d162022
maojj <maojj@YSC-MAOJJ.kingser.com>
2016-11-11 17:14:35 +0800

    V1.2 订单详情列表

--- okdeer-mall-order/src/main/java/com/okdeer/mall/order/service/impl/TradeOrderTraceServiceImpl.java
+++ okdeer-mall-order/src/main/java/com/okdeer/mall/order/service/impl/TradeOrderTraceServiceImpl.java
@@ -7,7 +7,6 @@ import java.util.List;
 import javax.annotation.Resource;
 
 import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import com.okdeer.archive.store.enums.ResultCodeEnum;
