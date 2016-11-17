ff5899cdb9fc9ba81d70eb8ce363127b8ed20b17
tuzhd <tuzhd@yschome.com>
2016-11-17 20:21:22 +0800

    APPTAB换肤定时任务

--- okdeer-mall-operate/src/main/java/com/okdeer/mall/operate/skinmanager/service/SkinManagerService.java
+++ okdeer-mall-operate/src/main/java/com/okdeer/mall/operate/skinmanager/service/SkinManagerService.java
@@ -6,10 +6,15 @@
  */
 package com.okdeer.mall.operate.skinmanager.service;
 
+import java.util.Date;
+
+import org.springframework.transaction.annotation.Transactional;
+
 import com.okdeer.base.common.utils.PageUtils;
 import com.okdeer.base.service.IBaseService;
 import com.okdeer.mall.operate.dto.SkinManagerDto;
 import com.okdeer.mall.operate.dto.SkinManagerParamDto;
+import com.okdeer.mall.operate.enums.SkinManagerStatus;
 
 /**
  * ClassName: ISkinManagerService 
@@ -100,4 +105,27 @@ public interface SkinManagerService extends IBaseService {
 	 */
 	void add(SkinManagerDto skinManagerDto);
 	// End V1.2.0 added by maojj 2016-11-16
+	
+	
+	/**
+	 * 执行换肤活动管理 JOB 任务
+	 * @Description: TODO   
+	 * @return void  
+	 * @throws
+	 * @author tuzhd
+	 * @date 2016年11月16日
+	 */
+	public void processSkinActivityJob();
+	
+	/**
+	 * @Description: 根据id换肤活动管理状态
+	 * @param id
+	 * @param status 活动状态 0 未开始 ，1：进行中2:已结束 
+	 * @param updateUserId 修改人
+	 * @param updateTime 修改时间
+	 * @throws Exception
+	 * @author tuzhiding
+	 * @date 2016年11月12日
+	 */
+	public void updateStatusById(String id, SkinManagerStatus status, String updateUserId, Date updateTime) throws Exception;
 }
