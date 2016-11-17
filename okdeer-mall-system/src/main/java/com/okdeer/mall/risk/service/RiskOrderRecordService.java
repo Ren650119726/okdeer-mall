e2f4f6c112c3016c9fa096059931c00d40b49e0a
guocp <guocp@okdeer.com>
2016-11-17 14:09:11 +0800

    风控订单记录及风控触发条件

---
+++ okdeer-mall-system/src/main/java/com/okdeer/mall/risk/service/RiskOrderRecordService.java
@@ -0,0 +1,26 @@
+/** 
+ *@Project: okdeer-mall-system 
+ *@Author: guocp
+ *@Date: 2016年11月17日 
+ *@Copyright: ©2014-2020 www.okdeer.com Inc. All rights reserved. 
+ */    
+package com.okdeer.mall.risk.service;
+
+import com.okdeer.base.service.IBaseService;
+import com.okdeer.mall.risk.entity.RiskOrderRecord;
+
+/**
+ * ClassName: RiskOrderRecordService 
+ * @Description: 风控记录接口
+ * @author guocp
+ * @date 2016年11月17日
+ *
+ * =================================================================================================
+ *     Task ID			  Date			     Author		      Description
+ * ----------------+----------------+-------------------+-------------------------------------------
+ *
+ */
+
+public interface RiskOrderRecordService extends IBaseService{
+
+}
