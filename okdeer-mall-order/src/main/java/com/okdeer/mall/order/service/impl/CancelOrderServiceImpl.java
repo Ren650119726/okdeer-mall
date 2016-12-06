7a3fcabafedc7ee559a5c1d7d403777667e2b13d
zengjz <zengjz@yschome.com>
2016-12-06 13:09:09 +0800

    增加释放代金卷逻辑判断

--- okdeer-mall-order/src/main/java/com/okdeer/mall/order/service/impl/CancelOrderServiceImpl.java
+++ okdeer-mall-order/src/main/java/com/okdeer/mall/order/service/impl/CancelOrderServiceImpl.java
@@ -209,10 +209,14 @@ public class CancelOrderServiceImpl implements CancelOrderService {
 				throw new Exception(ORDER_STATUS_CHANGE);
 			}
 
-			if (tradeOrder.getActivityType() == ActivityTypeEnum.VONCHER
-					&& OrderCancelType.CANCEL_BY_BUYER != tradeOrder.getCancelType()) {
-				// 释放所有代金卷
-				activityCouponsRecordService.updateUseStatus(tradeOrder.getId());
+			if (tradeOrder.getActivityType() == ActivityTypeEnum.VONCHER) {
+				if (tradeOrder.getType() == OrderTypeEnum.PHYSICAL_ORDER
+						|| OrderStatusEnum.DROPSHIPPING != oldOrder.getStatus()
+						|| (OrderCancelType.CANCEL_BY_BUYER != tradeOrder.getCancelType())) {
+					// 如果实物订单或者不是待服务状态或者不是用户取消的就需要释放代金卷
+					// 释放所有代金卷
+					activityCouponsRecordService.updateUseStatus(tradeOrder.getId());
+				}
 			} else if (tradeOrder.getActivityType() == ActivityTypeEnum.GROUP_ACTIVITY) {
 				// 团购活动释放限购数量
 				activityGroupRecordService.updateDisabledByOrderId(tradeOrder.getId());
