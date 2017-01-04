fe15d42c62e9db178aecdc4349a21b8960675b71
mengshijian <meshji66@163.com>
2017-01-04 15:55:43 +0800

    代码合并

--- okdeer-mall-activity/src/main/java/com/okdeer/mall/activity/coupons/service/impl/ActivitySaleServiceImpl.java
+++ okdeer-mall-activity/src/main/java/com/okdeer/mall/activity/coupons/service/impl/ActivitySaleServiceImpl.java
@@ -91,8 +91,8 @@ public class ActivitySaleServiceImpl implements ActivitySaleServiceApi, Activity
 			// 先保存特惠主对象
 			activitySaleMapper.save(activitySale);
 			// 库存同步--库存出错的几率更大。先处理库存
-			this.syncGoodsStockBatch(asgList, activitySale.getCreateUserId(), activitySale.getStoreId(),
-					StockOperateEnum.ACTIVITY_STOCK, rpcIdByStockList);
+			/*this.syncGoodsStockBatch(asgList, activitySale.getCreateUserId(), activitySale.getStoreId(),
+					StockOperateEnum.ACTIVITY_STOCK, rpcIdByStockList);*/
 			// 再保存特惠商品列表
 			for (ActivitySaleGoods a : asgList) {
 				a.setDisabled(Disabled.valid);
@@ -566,8 +566,21 @@ public class ActivitySaleServiceImpl implements ActivitySaleServiceApi, Activity
 	public ActivitySale findActivitySaleByStoreId(String storeId,
 			Integer activiType) {
 		if(StringUtils.isNotBlank(storeId) && activiType != null){
-			return activitySaleMapper.findByActivitySaleByStoreId(storeId, activiType);
+			return activitySaleMapper.findByActivitySaleByStoreId(storeId, activiType, null);
 		}
 		return new ActivitySale();
+	}
+	
+	/**
+	 * (non-Javadoc)
+	 * @see com.okdeer.mall.activity.coupons.service.ActivitySaleServiceApi#findLowPriceActivitySaleByStoreId(java.lang.String)
+	 */
+	@Override
+	public ActivitySale findLowPriceActivitySaleByStoreId(String storeId) throws Exception {
+		if (StringUtils.isBlank(storeId)) {
+			return null;
+		}
+		return activitySaleMapper.findByActivitySaleByStoreId(storeId, ActivityTypeEnum.LOW_PRICE.ordinal(),
+				ActivitySaleStatus.ing.getValue());
 	}
 }
