6218b2374ec0afe7f753655ab2cc3062431490da
mengshijian <meshji66@163.com>
2017-01-06 15:57:32 +0800

    低价抢购对外服务接口提取

---
+++ okdeer-mall-activity/src/main/java/com/okdeer/mall/activity/coupons/api/ActivitySaleServiceApiImpl.java
@@ -0,0 +1,31 @@
+package com.okdeer.mall.activity.coupons.api;
+
+import java.util.List;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.okdeer.mall.activity.coupons.entity.ActivitySale;
+import com.okdeer.mall.activity.coupons.entity.ActivitySaleGoods;
+import com.okdeer.mall.activity.coupons.service.ActivitySaleELServiceApi;
+import com.okdeer.mall.activity.coupons.service.ActivitySaleService;
+
+@Service(version = "1.0.0", interfaceName = "com.okdeer.mall.activity.coupons.service.ActivitySaleELServiceApi")
+public class ActivitySaleServiceApiImpl implements ActivitySaleELServiceApi {
+	
+	private ActivitySaleService activitySaleService;
+	@Override
+	public void save(ActivitySale activitySale, List<ActivitySaleGoods> asgList)
+			throws Exception {
+		activitySaleService.save(activitySale, asgList);
+	}
+
+	@Override
+	public void update(ActivitySale ActivitySale,
+			List<ActivitySaleGoods> asgList) throws Exception {
+		activitySaleService.update(ActivitySale, asgList);
+	}
+	
+	@Override
+	public void closeSaleGoods(String saleGoodsId) throws Exception {
+		activitySaleService.closeSaleGoods(saleGoodsId);
+	}
+}
