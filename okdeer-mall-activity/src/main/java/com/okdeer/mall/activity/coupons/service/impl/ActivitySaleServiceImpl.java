01f4e523362e2fdbf3e7c3210575f18e6760d3df
mengshijian <meshji66@163.com>
2017-01-08 19:29:51 +0800

    合并代码a

--- okdeer-mall-activity/src/main/java/com/okdeer/mall/activity/coupons/service/impl/ActivitySaleServiceImpl.java
+++ okdeer-mall-activity/src/main/java/com/okdeer/mall/activity/coupons/service/impl/ActivitySaleServiceImpl.java
@@ -41,10 +41,9 @@ import com.okdeer.mall.activity.coupons.service.ActivitySaleServiceApi;
 import com.okdeer.mall.system.mq.RollbackMQProducer;
 
 /**
- * 
  * ClassName: ActivitySaleServiceImpl 
  * @Description: 特惠活动服务
- * @author zengj
+ * @author zengj 
  * @date 2016年9月7日
  *
  * =================================================================================================
@@ -177,6 +176,8 @@ public class ActivitySaleServiceImpl implements ActivitySaleServiceApi, Activity
 				adjustDetailVo.setNum(goods.getSaleStock());
 				if(entity.getSpuTypeEnum() == SpuTypeEnum.assembleSpu){
 					//如果是组合商品,不需要同步进销存的库存
+					adjustDetailVo.setGoodsName(entity.getName());
+					adjustDetailVo.setBarCode(entity.getBarCode());
 					assDtailList.add(adjustDetailVo);
 				}else{
 					/*************新增字段 start **************/
@@ -189,14 +190,15 @@ public class ActivitySaleServiceImpl implements ActivitySaleServiceApi, Activity
 				}
 			}
 		}
-		stockAdjustVo.setAdjustDetailList(adjustDetailList);
 		stockAdjustVo.setStockOperateEnum(soe);
 		//更新商城活动库存
 		if(assDtailList.size() > 0){
+			stockAdjustVo.setAdjustDetailList(assDtailList);
 			stockManagerServiceApi.updateStock(stockAdjustVo);
 		}
 		log.info("特惠活动时同步erp库存开始:");
 		if(adjustDetailList.size() > 0){
+			stockAdjustVo.setAdjustDetailList(adjustDetailList);
 			stockManagerJxcServiceApi.updateStock(stockAdjustVo);
 		}
 		log.info("特惠活动时同步erp完成:");
