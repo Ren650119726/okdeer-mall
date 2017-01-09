16b0680aa0e836c3c023caf936f9125e07a6c8d9
tangzj02 <tangzhj02@yschome.com>
2017-01-09 14:50:52 +0800

    地区重复逻辑判断修改

--- okdeer-mall-operate/src/main/java/com/okdeer/mall/operate/service/impl/ColumnHomeIconServiceImpl.java
+++ okdeer-mall-operate/src/main/java/com/okdeer/mall/operate/service/impl/ColumnHomeIconServiceImpl.java
@@ -175,7 +175,17 @@ public class ColumnHomeIconServiceImpl extends BaseServiceImpl implements Column
 		paramDto.setPlace(place);
 		// 设置排除自己
 		paramDto.setExcludeId(homeIconId);
+		paramDto.setTaskScope(SelectAreaType.nationwide);
+		// 根据位置查询设置范围为全国的数据
 		List<ColumnHomeIcon> list = homeIconMapper.findList(paramDto);
+		if (null != list && list.size() > 0) {
+			// 如果同一位置中有设置范围是全国的则表示已经重复
+			return true;
+		}
+		// 清空参数中的地区范围
+		paramDto.setTaskScope(null);
+		// 查询位置设置范围为各省和城市的数据
+		list = homeIconMapper.findList(paramDto);
 		// 未查出数据则表示当前ICON位置还未发布过
 		if (null == list || list.size() == 0) {
 			return false;
