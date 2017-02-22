fe558ef372d4333f76f3d641e0de0598556acf1a
tangy <tangy@yschome.com>
2017-02-22 11:24:07 +0800

    活动库存同步调整

--- okdeer-mall-activity/src/main/java/com/okdeer/mall/activity/coupons/service/impl/ActivitySaleServiceImpl.java
+++ okdeer-mall-activity/src/main/java/com/okdeer/mall/activity/coupons/service/impl/ActivitySaleServiceImpl.java
@@ -241,7 +241,7 @@ public class ActivitySaleServiceImpl implements ActivitySaleServiceApi, Activity
 					/*************新增字段 end  **************/
 					//Begin V2.1.0 added by 标识同步商品参与特惠  tangy  2017-02-21
 					if(activitySale != null && activitySale.getType() == ActivityTypeEnum.SALE_ACTIVITIES){
-						adjustDetailVo.setPreference(true);
+						adjustDetailVo.setIsPreference(true);
 					}
 					//End added by tangy
 					adjustDetailList.add(adjustDetailVo);
@@ -537,7 +537,8 @@ public class ActivitySaleServiceImpl implements ActivitySaleServiceApi, Activity
 				// 手动关闭或者定时器结束都要把未卖完的数量释放库存
 				// 和erp同步库存
 				if(CollectionUtils.isNotEmpty(saleGoodsList)){
-					this.syncGoodsStockBatch(saleGoodsList, null, "", storeId, StockOperateEnum.ACTIVITY_END, rpcIdByStockList);
+					StockOperateEnum stockOperateEnum = activityType == ActivityTypeEnum.SALE_ACTIVITIES.ordinal() ? StockOperateEnum.OVER_SALE_ORDER_INTEGRAL : StockOperateEnum.OVER_SALE_ORDER_EVENT;
+					this.syncGoodsStockBatch(saleGoodsList, null, "", storeId, stockOperateEnum, rpcIdByStockList);
 				}
 			}
 		} catch (Exception e) {
