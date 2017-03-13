ad0595a33f582013bd13d26da321b5de050326b7
wangf01@yschome.com <wangf01@yschome.com>
2017-03-13 17:47:32 +0800

    运营栏目鹿小宝功能底层代码编写

---
+++ okdeer-mall-operate/src/main/java/com/okdeer/mall/operate/entity/ColumnOperationBo.java
@@ -0,0 +1,15 @@
+package com.okdeer.mall.operate.entity;
+
+/**
+ * ClassName: ColumnOperationBo
+ *
+ * @author wangf01
+ * @Description: 运营栏目-bo
+ * @date 2017年3月9日
+ * <p>
+ * =================================================================================================
+ * Task ID			  Date			     Author		      Description
+ * ----------------+----------------+-------------------+-------------------------------------------
+ */
+public class ColumnOperationBo extends ColumnOperation{
+}
