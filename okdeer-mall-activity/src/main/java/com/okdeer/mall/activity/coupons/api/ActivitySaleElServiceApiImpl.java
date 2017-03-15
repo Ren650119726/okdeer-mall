0c5014f628e6f484af38ecefcdf0166cd44b8731
tangy <tangy@yschome.com>
2017-03-15 15:07:13 +0800

    支持进行中的活动商品信息更改

--- okdeer-mall-activity/src/main/java/com/okdeer/mall/activity/coupons/api/ActivitySaleElServiceApiImpl.java
+++ okdeer-mall-activity/src/main/java/com/okdeer/mall/activity/coupons/api/ActivitySaleElServiceApiImpl.java
@@ -1,6 +1,25 @@
 
 package com.okdeer.mall.activity.coupons.api;
 
+import static com.okdeer.common.consts.ELTopicTagConstants.TAG_LOWPRICE_EL_UPDATE;
+import static com.okdeer.common.consts.ELTopicTagConstants.TAG_SALE_EL_DEL;
+import static com.okdeer.common.consts.ELTopicTagConstants.TAG_SALE_EL_UPDATE;
+import static com.okdeer.common.consts.ELTopicTagConstants.TAG_STOCK_EL_UPDATE;
+import static com.okdeer.common.consts.ELTopicTagConstants.TOPIC_GOODS_SYNC_EL;
+import static com.okdeer.common.consts.ELTopicTagConstants.TAG_SALE_LOWPRICE_EL_EDIT;
+import static com.okdeer.common.consts.StoreMenuTopicTagConstants.TAG_STORE_MENU_UPDATE;
+import static com.okdeer.mall.activity.coupons.enums.ActivityTypeEnum.LOW_PRICE;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.alibaba.dubbo.config.annotation.Service;
 import com.alibaba.rocketmq.common.message.Message;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -16,18 +35,6 @@ import com.okdeer.mall.activity.coupons.service.ActivitySaleGoodsServiceApi;
 import com.okdeer.mall.activity.coupons.service.ActivitySaleService;
 import com.okdeer.mall.activity.dto.ActivitySaleGoodsParamDto;
 import com.okdeer.mall.activity.service.ArchiveSendMsgService;
-import org.apache.commons.collections.CollectionUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import static com.okdeer.common.consts.ELTopicTagConstants.*;
-import static com.okdeer.common.consts.StoreMenuTopicTagConstants.TAG_STORE_MENU_UPDATE;
-import static com.okdeer.mall.activity.coupons.enums.ActivityTypeEnum.LOW_PRICE;
 
 /**
  * 
@@ -158,8 +165,19 @@ public class ActivitySaleElServiceApiImpl implements ActivitySaleELServiceApi {
     	activitySaleService.updateSaleStock(activitySale, activitySaleGoods);
     	//同步库存到搜索引擎
     	ActivityMessageParamDto paramDto = new ActivityMessageParamDto();
-    	List<String> skuIdList = Arrays.asList(activitySaleGoods.getGoodsSkuId());
+    	List<String> skuIdList = Arrays.asList(activitySaleGoods.getStoreSkuId());
     	paramDto.setSkuIds(skuIdList);
     	archiveSendMsgService.structureProducerELGoods(paramDto, TAG_STOCK_EL_UPDATE);
+    }
+    
+    @Override
+    public void updateActivitySaleGoods(ActivitySaleGoods activitySaleGoods) throws Exception {
+    	activitySaleGoodsServiceApi.updateActivitySaleGoods(activitySaleGoods);    	
+    	// 更新搜索引擎
+    	List<String> skuIds = new ArrayList<String>();
+    	skuIds.add(activitySaleGoods.getStoreSkuId());
+    	ActivityMessageParamDto paramDto = new ActivityMessageParamDto();
+        paramDto.setSkuIds(skuIds);
+        archiveSendMsgService.structureProducerELGoods(paramDto, TAG_SALE_LOWPRICE_EL_EDIT);
     }
 }
