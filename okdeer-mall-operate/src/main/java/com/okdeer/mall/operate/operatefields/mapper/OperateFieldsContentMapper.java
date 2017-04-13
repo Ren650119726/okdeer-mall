9cad2fa8ad435569dc2165354b8d038b6ddd489a
zengjz <zengjz@yschome.com>
2017-04-13 20:27:10 +0800

    运营栏位功能开发

--- okdeer-mall-operate/src/main/java/com/okdeer/mall/operate/operatefields/mapper/OperateFieldsContentMapper.java
+++ okdeer-mall-operate/src/main/java/com/okdeer/mall/operate/operatefields/mapper/OperateFieldsContentMapper.java
@@ -6,8 +6,39 @@
  */
 package com.okdeer.mall.operate.operatefields.mapper;
 
+import java.util.List;
+
 import com.okdeer.base.dal.IBaseMapper;
+import com.okdeer.mall.operate.operatefields.entity.OperateFieldsContent;
 
+/**
+ * ClassName: OperateFieldsContentMapper 
+ * @Description: TODO
+ * @author zengjizu
+ * @date 2017年4月13日
+ *
+ * =================================================================================================
+ *     Task ID			  Date			     Author		      Description
+ * ----------------+----------------+-------------------+-------------------------------------------
+ *
+ */
 public interface OperateFieldsContentMapper extends IBaseMapper {
-
+ 
+	/**
+	 * @Description: 根据fieldId查询列表
+	 * @param fieldId 栏位id
+	 * @return
+	 * @author zengjizu
+	 * @date 2017年4月13日
+	 */
+	List<OperateFieldsContent> findByFieldId(String fieldId);
+	
+	/**
+	 * @Description: 根据运营位id删除
+	 * @param fieldId 运营位id
+	 * @return
+	 * @author zengjizu
+	 * @date 2017年4月13日
+	 */
+	int deleteByFieldId(String fieldId);
 }
--- okdeer-mall-operate/src/main/java/com/okdeer/mall/operate/operatefields/mapper/OperateFieldsMapper.java
+++ okdeer-mall-operate/src/main/java/com/okdeer/mall/operate/operatefields/mapper/OperateFieldsMapper.java
@@ -6,8 +6,64 @@
  */
 package com.okdeer.mall.operate.operatefields.mapper;
 
+
+import java.util.List;
+
 import com.okdeer.base.dal.IBaseMapper;
+import com.okdeer.mall.operate.dto.OperateFieldsQueryParamDto;
+import com.okdeer.mall.operate.enums.OperateFieldsType;
+import com.okdeer.mall.operate.operatefields.bo.OperateFieldsBo;
+import com.okdeer.mall.operate.operatefields.entity.OperateFields;
 
+/**
+ * ClassName: OperateFieldsMapper 
+ * @Description: TODO
+ * @author zengjizu
+ * @date 2017年4月13日
+ *
+ * =================================================================================================
+ *     Task ID			  Date			     Author		      Description
+ * ----------------+----------------+-------------------+-------------------------------------------
+ *
+ */
 public interface OperateFieldsMapper extends IBaseMapper {
-
+	
+	/**
+	 * @Description: 获取列表
+	 * @param queryParamDto 查询参数
+	 * @return 运营栏位列表
+	 * @author zengjizu
+	 * @date 2017年4月13日
+	 */
+	List<OperateFields> findList(OperateFieldsQueryParamDto queryParamDto);
+	
+	/**
+	 * @Description: 获取列表
+	 * @param queryParamDto 查询参数
+	 * @return  运营栏位列表带内容列表
+	 * @author zengjizu
+	 * @date 2017年4月13日
+	 */
+	List<OperateFieldsBo> findListWithContent(OperateFieldsQueryParamDto queryParamDto);
+	
+	/**
+	 * @Description: 查询最小排序值
+	 * @param type 运营栏位类型
+	 * @param businessId 业务id
+	 * @return
+	 * @author zengjizu
+	 * @date 2017年4月13日
+	 */
+	Integer queryMinSort(OperateFieldsType type,String businessId);
+	
+	/**
+	 * @Description: 查询需要对比的数据
+	 * @param id
+	 * @param sort
+	 * @return
+	 * @author zengjizu
+	 * @date 2017年4月13日
+	 */
+	OperateFields findCompareBySort(String id, int sort,int type);
+	
 }
