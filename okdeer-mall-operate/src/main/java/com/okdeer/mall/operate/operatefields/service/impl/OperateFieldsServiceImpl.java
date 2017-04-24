183a932bf87e3caa167a14a31082bfddee8919bf
zhangkn <zhangkn@yschome.com>
2017-04-24 16:08:01 +0800

    修改上移下移bug

--- okdeer-mall-operate/src/main/java/com/okdeer/mall/operate/operatefields/service/impl/OperateFieldsServiceImpl.java
+++ okdeer-mall-operate/src/main/java/com/okdeer/mall/operate/operatefields/service/impl/OperateFieldsServiceImpl.java
@@ -136,7 +136,7 @@ public class OperateFieldsServiceImpl extends BaseServiceImpl implements Operate
 		if (isUp) {
 			type = 1;
 		}
-		OperateFields compareOperateFields = operateFieldsMapper.findCompareBySort(operateFields.getId(), sort, type);
+		OperateFields compareOperateFields = operateFieldsMapper.findCompareBySort(operateFields,type);
 
 		if (compareOperateFields == null) {
 			throw new Exception("没有可以移动的数据");
