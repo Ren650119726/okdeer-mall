499c4b566bd334fd14057784e0c4bd719e3483d7
tuzhd <tuzhd@yschome.com>
2017-06-26 18:17:43 +0800

    添加梯度消费返券功能

--- okdeer-mall-order/src/main/java/com/okdeer/mall/order/service/impl/TradeOrderServiceImpl.java
+++ okdeer-mall-order/src/main/java/com/okdeer/mall/order/service/impl/TradeOrderServiceImpl.java
@@ -42,7 +42,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.integration.redis.util.RedisLockRegistry;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -378,9 +377,6 @@ public class TradeOrderServiceImpl implements TradeOrderService, TradeOrderServi
 	@Reference(version = "1.0.0", check = false)
 	private IStoreInfoExtServiceApi storeInfoExtService;
 	
-	@Autowired
-	private  RedisTemplate<String,Boolean> redisTemplate;
-
 	/**
 	 * 订单收货信息
 	 */
