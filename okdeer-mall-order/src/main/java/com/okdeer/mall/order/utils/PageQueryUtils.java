fbd8aaa7f8e01410b558a09aac4bb61c965d2fa3
zengjz <zengjz@yschome.com>
2017-07-07 14:47:24 +0800

    分页工具类修改

--- okdeer-mall-order/src/main/java/com/okdeer/mall/order/utils/PageQueryUtils.java
+++ okdeer-mall-order/src/main/java/com/okdeer/mall/order/utils/PageQueryUtils.java
@@ -22,7 +22,7 @@ public class PageQueryUtils {
 			int page = ids.size() % pageSize == 0 ? ids.size() / pageSize : ids.size() / pageSize + 1;
 			for (int i = 0; i < page; i++) {
 				int fromIndex = i * pageSize;
-				int toIndex = fromIndex + pageSize - 1;
+				int toIndex = fromIndex + pageSize;
 				if (toIndex > ids.size()) {
 					toIndex = ids.size();
 				}
