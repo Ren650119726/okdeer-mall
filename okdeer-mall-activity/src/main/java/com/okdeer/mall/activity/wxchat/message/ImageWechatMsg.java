22de5a62d44f68b52098ef7b0bc9cfe730bbeeb9
zengjz <zengjz@yschome.com>
2017-08-03 15:21:46 +0800

    海报活动开发

--- okdeer-mall-activity/src/main/java/com/okdeer/mall/activity/wxchat/message/ImageWechatMsg.java
+++ okdeer-mall-activity/src/main/java/com/okdeer/mall/activity/wxchat/message/ImageWechatMsg.java
@@ -2,28 +2,30 @@
 package com.okdeer.mall.activity.wxchat.message;
 
 import java.util.Date;
+import java.util.List;
 
 import com.okdeer.mall.activity.wxchat.annotation.XStreamCDATA;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamImplicit;
 
 @XStreamAlias("xml")
 public class ImageWechatMsg extends WechatMsg {
 
-	@XStreamAlias("MediaId")
+	@XStreamImplicit(keyFieldName="Image",itemFieldName="MediaId")
 	@XStreamCDATA
-	private String mediaId;
+	private List<String> mediaId;
 
 	public ImageWechatMsg() {
 		setMsgType("image");
-		setCreateTime(Long.toString((new Date().getTime())/1000));
-		
+		setCreateTime(Long.toString((new Date().getTime()) / 1000));
+
 	}
 
-	public String getMediaId() {
+	public List<String> getMediaId() {
 		return mediaId;
 	}
 
-	public void setMediaId(String mediaId) {
+	public void setMediaId(List<String> mediaId) {
 		this.mediaId = mediaId;
 	}
 
