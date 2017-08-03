49e53e6e9a5aa8438838360ce152c7bd1433be92
zengjz <zengjz@yschome.com>
2017-08-03 15:07:28 +0800

    微信海报活动开发

--- okdeer-mall-activity/src/main/java/com/okdeer/mall/activity/wxchat/service/impl/WechatMsgProcessServiceImpl.java
+++ okdeer-mall-activity/src/main/java/com/okdeer/mall/activity/wxchat/service/impl/WechatMsgProcessServiceImpl.java
@@ -1,6 +1,8 @@
 
 package com.okdeer.mall.activity.wxchat.service.impl;
 
+import java.io.Writer;
+import java.lang.reflect.Field;
 import java.util.Map;
 
 import org.slf4j.Logger;
@@ -9,11 +11,17 @@ import org.springframework.stereotype.Service;
 
 import com.google.common.collect.Maps;
 import com.okdeer.common.exception.MallApiException;
+import com.okdeer.mall.activity.wxchat.annotation.XStreamCDATA;
 import com.okdeer.mall.activity.wxchat.service.WechatMsgHandlerService;
 import com.okdeer.mall.activity.wxchat.service.WechatMsgProcessService;
 import com.okdeer.mall.activity.wxchat.util.WxchatUtils;
 import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.core.util.QuickWriter;
+import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
 import com.thoughtworks.xstream.io.xml.Dom4JDriver;
+import com.thoughtworks.xstream.io.xml.PrettyPrintWriter;
+import com.thoughtworks.xstream.io.xml.XppDriver;
 
 @Service
 public class WechatMsgProcessServiceImpl implements WechatMsgProcessService, WechatMsgHandlerService {
@@ -33,7 +41,7 @@ public class WechatMsgProcessServiceImpl implements WechatMsgProcessService, Wec
 			logger.warn("没有找到相应的消息处理类：msgType={}", msgType);
 			return null;
 		}
-		XStream xStream = new XStream(new Dom4JDriver());
+		XStream xStream = createXstream();
 		xStream.autodetectAnnotations(true);
 		xStream.alias("xml", wechatMsgHandler.getRequestClass());
 		Object requestObj = xStream.fromXML(requestXml);
@@ -74,6 +82,84 @@ public class WechatMsgProcessServiceImpl implements WechatMsgProcessService, Wec
 			return msgType;
 		}
 		return msgType;
+	}
+
+	private XStream createXstream() {
+		return new XStream(new XppDriver() {
+
+			@Override
+			public HierarchicalStreamWriter createWriter(Writer out) {
+				return new PrettyPrintWriter(out) {
+
+					boolean cdata = false;
+
+					Class<?> targetClass = null;
+
+					@Override
+					public void startNode(String name, @SuppressWarnings("rawtypes") Class clazz) {
+						super.startNode(name, clazz);
+						// 业务处理，对于用XStreamCDATA标记的Field，需要加上CDATA标签
+						if (!name.equals("xml")) {// 代表当前处理节点是class，用XstreamAlias把class的别名改成xml，下面的代码片段有提到
+							cdata = needCDATA(targetClass, name);
+						} else {
+							targetClass = clazz;
+						}
+					}
+
+					@Override
+					protected void writeText(QuickWriter writer, String text) {
+						if (cdata) {
+							writer.write(cDATA(text));
+						} else {
+							writer.write(text);
+						}
+					}
+
+				};
+			}
+		});
+	}
+
+	private String cDATA(String text) {
+		return "<![CDATA[" + text + "]]>";
+	}
+
+	private boolean needCDATA(Class<?> targetClass, String fieldAlias) {
+		boolean cdata = false;
+		// first, scan self
+		cdata = existsCDATA(targetClass, fieldAlias);
+		if (cdata)
+			return cdata;
+		// if cdata is false, scan supperClass until java.lang.Object
+		Class<?> superClass = targetClass.getSuperclass();
+		while (!superClass.equals(Object.class)) {
+			cdata = existsCDATA(superClass, fieldAlias);
+			if (cdata) {
+				return cdata;
+			}
+			superClass = superClass.getSuperclass();
+		}
+		return false;
+	}
+
+	private boolean existsCDATA(Class<?> clazz, String fieldAlias) {
+		// scan fields
+		Field[] fields = clazz.getDeclaredFields();
+		for (Field field : fields) {
+			// 1. exists XStreamCDATA
+			if (field.getAnnotation(XStreamCDATA.class) != null) {
+				XStreamAlias xStreamAlias = field.getAnnotation(XStreamAlias.class);
+				// 2. exists XStreamAlias
+				if (null != xStreamAlias) {
+					if (fieldAlias.equals(xStreamAlias.value()))// matched
+						return true;
+				} else {// not exists XStreamAlias
+					if (fieldAlias.equals(field.getName()))
+						return true;
+				}
+			}
+		}
+		return false;
 	}
 
 }
