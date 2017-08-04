8126411c7efd80402498353221b3082d439eb628
zengjz <zengjz@yschome.com>
2017-08-04 18:19:40 +0800

    微信海报活动开发

--- okdeer-mall-activity/src/main/java/com/okdeer/mall/activity/wxchat/service/impl/AbstractEventWechatMsgService.java
+++ okdeer-mall-activity/src/main/java/com/okdeer/mall/activity/wxchat/service/impl/AbstractEventWechatMsgService.java
@@ -5,19 +5,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.okdeer.mall.activity.wxchat.config.WechatConfig;
 import com.okdeer.mall.activity.wxchat.enums.MsgTypeEnum;
+import com.okdeer.mall.activity.wxchat.util.WxchatUtils;
 
 public abstract class AbstractEventWechatMsgService extends WechatMsgHandler {
 
 	@Autowired
 	protected WechatConfig wechatConfig;
 
-
 	abstract String getEvent();
 
 	@Override
 	String getMsgTypeEvent() {
 
-		return MsgTypeEnum.EVENT + getEvent();
+		return MsgTypeEnum.EVENT + WxchatUtils.JOIN + getEvent();
 	}
 
 }
