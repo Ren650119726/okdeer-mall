579271fdaacc0a636f9218ba28a30baa71b0ed68
zengjz <zengjz@yschome.com>
2017-08-04 16:38:45 +0800

    海报活动开发

--- okdeer-mall-activity/src/main/java/com/okdeer/mall/activity/wxchat/service/impl/WechatUserServiceImpl.java
+++ okdeer-mall-activity/src/main/java/com/okdeer/mall/activity/wxchat/service/impl/WechatUserServiceImpl.java
@@ -2,12 +2,15 @@
 package com.okdeer.mall.activity.wxchat.service.impl;
 
 import java.util.Date;
+import java.util.List;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.okdeer.base.common.utils.UuidUtils;
 import com.okdeer.base.common.utils.mapper.BeanMapper;
 import com.okdeer.base.dal.IBaseMapper;
 import com.okdeer.base.service.BaseServiceImpl;
@@ -19,12 +22,12 @@ import com.okdeer.mall.activity.wxchat.service.WechatUserService;
 
 @Service
 public class WechatUserServiceImpl extends BaseServiceImpl implements WechatUserService {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(WechatUserServiceImpl.class);
-	
+
 	@Autowired
 	private WechatUserMapper wechatUserMapper;
-	
+
 	@Autowired
 	private WechatService wechatService;
 
@@ -47,21 +50,33 @@ public class WechatUserServiceImpl extends BaseServiceImpl implements WechatUser
 			WechatUserInfo wechatUserInfo = wechatService.getUserInfo(openid);
 			if (wechatUser == null) {
 				// 用户不存在
-				wechatUser = new WechatUser();
-				BeanMapper.copy(wechatUserInfo, wechatUser);
-				wechatUser.setSubscribeTime(new Date(wechatUserInfo.getSubscribeTime() * 1000));
+				wechatUser = createWechatUser(wechatUserInfo);
+				wechatUser.setId(UuidUtils.getUuid());
 				wechatUserMapper.add(wechatUser);
 			} else {
 				// 已经存在,更新用户信息
-				WechatUser updateInfo = new WechatUser();
-				BeanMapper.copy(wechatUserInfo, updateInfo);
-				wechatUser.setSubscribeTime(new Date(wechatUserInfo.getSubscribeTime() * 1000));
+				WechatUser updateInfo = createWechatUser(wechatUserInfo);
 				updateInfo.setId(wechatUser.getId());
 				wechatUserMapper.update(updateInfo);
 			}
 		} catch (Exception e) {
 			logger.error("查询用户信息出错", e);
 		}
+	}
+
+	private WechatUser createWechatUser(WechatUserInfo wechatUserInfo) {
+		WechatUser wechatUser = new WechatUser();
+		BeanMapper.copy(wechatUserInfo, wechatUser);
+		wechatUser.setSubscribeTime(new Date(wechatUserInfo.getSubscribeTime() * 1000));
+		List<String> tagList = wechatUserInfo.getTagids();
+		if (CollectionUtils.isNotEmpty(tagList)) {
+			StringBuilder tagids = new StringBuilder();
+			for (String tagid : tagList) {
+				tagids.append(tagid + ",");
+			}
+			wechatUser.setTagids(tagids.toString());
+		}
+		return wechatUser;
 	}
 
 }
