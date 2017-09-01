00898f3b6fa30be6e72b470e5b6b10dfca3be9a9
tuzhd <tuzhd@yschome.com>
2017-09-01 15:46:55 +0800

    修改会员卡取消功能

--- okdeer-mall-order/src/main/java/com/okdeer/mall/order/api/MemberCardOrderApiImpl.java
+++ okdeer-mall-order/src/main/java/com/okdeer/mall/order/api/MemberCardOrderApiImpl.java
@@ -81,7 +81,7 @@ public class MemberCardOrderApiImpl implements MemberCardOrderApi {
      * @Description: 根据订单id取消订单,已提交订单不能清除，会导致用户支付无法对上账
      * @param orderId 订单id
      * @paran userId 用户id
-     * @param isAppUser 是否是后台用户   
+     * @param isAppUser 是否是app用户   
      * @return void  
      * @author tuzhd
      * @date 2017年8月10日
