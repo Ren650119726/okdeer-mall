c0a00a0be1078c035229357c5c8a303cc02ac2b4
mengsj <mengsj@yml-mengsj.kingser.com>
2017-09-04 14:36:44 +0800

    接口调整，修改了参数

--- okdeer-mall-test/src/test/java/com/okdeer/mall/activity/nadvert/service/ActivityH5AdvertContentGoodsServiceTest.java
+++ okdeer-mall-test/src/test/java/com/okdeer/mall/activity/nadvert/service/ActivityH5AdvertContentGoodsServiceTest.java
@@ -3,8 +3,8 @@ package com.okdeer.mall.activity.nadvert.service;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.okdeer.archive.goods.store.dto.GoodsStoreActivitySkuDto;
 import com.okdeer.base.common.utils.PageUtils;
-import com.okdeer.mall.activity.nadvert.entity.ActivityH5AdvertContentGoods;
 import com.okdeer.mall.base.BaseServiceTest;
 
 public class ActivityH5AdvertContentGoodsServiceTest extends BaseServiceTest {
@@ -15,7 +15,7 @@ public class ActivityH5AdvertContentGoodsServiceTest extends BaseServiceTest {
 		String storeId = "558c8935276511e6aaff00163e010eb1";
 		String activityId = "8a94e7c75e1da75e015e1daa8fa00004";
 		String contentId = "8a94e7c75e1da75e015e1daa8fc90009";
-		PageUtils<ActivityH5AdvertContentGoods> page = goodsService.findBldGoodsByActivityId(storeId, activityId, contentId, 1, 10);
+		PageUtils<GoodsStoreActivitySkuDto> page = goodsService.findBldGoodsByActivityId(storeId, activityId, contentId, 1, 10);
 		System.out.println(page.getList().size());
 	}
 }
