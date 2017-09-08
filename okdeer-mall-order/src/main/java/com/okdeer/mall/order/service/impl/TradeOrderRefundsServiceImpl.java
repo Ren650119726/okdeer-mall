452f040da334bae91159eca7f1f8abda2d7b5264
tuzhd <tuzhd@yschome.com>
2017-09-08 19:18:51 +0800

    还原

--- okdeer-mall-order/src/main/java/com/okdeer/mall/order/service/impl/TradeOrderRefundsServiceImpl.java
+++ okdeer-mall-order/src/main/java/com/okdeer/mall/order/service/impl/TradeOrderRefundsServiceImpl.java
@@ -1066,7 +1066,6 @@ public class TradeOrderRefundsServiceImpl
 		}
 
 		TradeOrderRefunds refunds = this.findById(refundsId);
-		JSONObject.fromObject(refunds);
 		List<TradeOrderRefundsItem> refundItemList = tradeOrderRefundsItemService
 				.getTradeOrderRefundsItemByRefundsId(refundsId);
 		refunds.setTradeOrderRefundsItem(refundItemList);
