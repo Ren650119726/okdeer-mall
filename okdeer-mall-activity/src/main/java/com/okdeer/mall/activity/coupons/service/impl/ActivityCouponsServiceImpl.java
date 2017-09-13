63b00126f7a174aa7df542d1967185f426c6ca48
zhangkn <zhangkn@yschome.com>
2017-09-13 10:20:00 +0800

    修改bug

--- okdeer-mall-activity/src/main/java/com/okdeer/mall/activity/coupons/service/impl/ActivityCouponsServiceImpl.java
+++ okdeer-mall-activity/src/main/java/com/okdeer/mall/activity/coupons/service/impl/ActivityCouponsServiceImpl.java
@@ -414,7 +414,7 @@ public class ActivityCouponsServiceImpl implements ActivityCouponsServiceApi, Ac
 		// 先把老数据删掉
 		activityCouponsRandCodeMapper.deleteByCouponsId(coupons.getId());
 		// 如果代金券设置了需要生成随机码，则根据代金券的发行量生成随机码
-		if (coupons.getIsRandCode() == WhetherEnum.whether.ordinal()) {
+		if (coupons.getIsRandCode() != null && coupons.getIsRandCode() == WhetherEnum.whether.ordinal()) {
 			// (由于新增和修改 接收的对象用的不是同一个,只能重新转换一下)
 			ActivityCoupons activitycoupons = new ActivityCoupons();
 			BeanMapper.copy(coupons, activitycoupons);
