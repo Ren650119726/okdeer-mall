10c0c267bd7e4802dbd71a656501bf1e529b9bba
zengjz <zengjz@yschome.com>
2017-09-23 16:56:35 +0800

    初始化分布式锁修改

--- okdeer-mall-service/src/main/java/com/okdeer/mall/config/RedisLockRegistryConfigure.java
+++ okdeer-mall-service/src/main/java/com/okdeer/mall/config/RedisLockRegistryConfigure.java
@@ -3,13 +3,25 @@
  *@Author: guocp
  *@Date: 2017年5月3日 
  *@Copyright: ©2014-2020 www.okdeer.com Inc. All rights reserved. 
- */    
+ */
+
 package com.okdeer.mall.config;
 
+import java.util.Date;
+import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.integration.redis.util.RedisLockRegistry;
+import org.springframework.integration.support.locks.LockRegistry;
+import org.springframework.util.Assert;
+
+import com.google.common.collect.Maps;
 
 /**
  * Redis锁
@@ -24,10 +36,108 @@ import org.springframework.integration.redis.util.RedisLockRegistry;
  */
 @Configuration
 public class RedisLockRegistryConfigure {
-	
+
+	private static Logger logger = LoggerFactory.getLogger(RedisLockRegistryConfigure.class);
+
+	private static final long DEFAULT_EXPIRE_AFTER = 60000;
+
 	@Bean
-	public RedisLockRegistry redisLockRegistry(RedisConnectionFactory factory){
-		RedisLockRegistry lockRegistry = new RedisLockRegistry(factory, "MALL-SERVICE");
+	public RedisLockRegistry redisLockRegistry(RedisConnectionFactory factory) {
+		RedisLockRegistry lockRegistry = new RedisLockRegistry(factory, "MALL-SERVICE", DEFAULT_EXPIRE_AFTER,
+				new RedisDefaultLockRegistry());
 		return lockRegistry;
 	}
+
+	private class RedisDefaultLockRegistry implements LockRegistry {
+
+		/**
+		 * 健值过期时间
+		 */
+		private static final long KEY_EXPIRE_TIME = 600000;
+
+		Map<Object, LockInfo> lockTable = Maps.newConcurrentMap();
+
+		public RedisDefaultLockRegistry() {
+			Thread thread = new Thread(new RemoveExpireLockTask());
+			thread.setDaemon(true);
+			thread.start();
+		}
+
+		private class RemoveExpireLockTask implements Runnable {
+			
+			@Override
+			public void run() {
+				while(true){
+					Map<Object, LockInfo> lockTable = RedisDefaultLockRegistry.this.lockTable;
+					for (Object localkey : lockTable.keySet()) {
+						LockInfo topicConfig = lockTable.get(localkey);
+						if (isExpire(topicConfig.getLastGetTime())) {
+							// 健已经超过10分钟没有使用了，删除掉
+							LockInfo lockInfo = lockTable.remove(localkey);
+							logger.info("{}对应的本地锁已经删除，最后一次使用时间:{}", localkey, lockInfo.getLastGetTime());
+						}
+					}
+					try {
+						wait(60000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+						Thread.currentThread().interrupt();
+						break;
+					}
+				}
+			}
+			private boolean isExpire(Date date) {
+				return System.currentTimeMillis() > (date.getTime() + KEY_EXPIRE_TIME);
+			}
+		}
+
+		@Override
+		public Lock obtain(Object lockKey) {
+			Assert.notNull(lockKey, "'lockKey' must not be null");
+			LockInfo lockInfo = lockTable.get(lockKey);
+			if (lockInfo == null) {
+				lockInfo = new LockInfo();
+				lockInfo.setLastGetTime(new Date());
+				lockInfo.setLockKey(lockKey);
+				lockInfo.setLock(new ReentrantLock());
+				lockTable.put(lockKey, lockInfo);
+			}
+			return lockInfo.getLock();
+		}
+	}
+
+	private class LockInfo {
+
+		private Object lockKey;
+
+		private Lock lock;
+
+		private Date lastGetTime;
+
+		public Object getLockKey() {
+			return lockKey;
+		}
+
+		public void setLockKey(Object lockKey) {
+			this.lockKey = lockKey;
+		}
+
+		public Lock getLock() {
+			return lock;
+		}
+
+		public void setLock(Lock lock) {
+			this.lock = lock;
+		}
+
+		public Date getLastGetTime() {
+			return lastGetTime;
+		}
+
+		public void setLastGetTime(Date lastGetTime) {
+			this.lastGetTime = lastGetTime;
+		}
+
+	}
+
 }
