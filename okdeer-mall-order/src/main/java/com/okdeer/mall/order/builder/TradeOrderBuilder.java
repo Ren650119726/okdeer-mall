f6a71e95da2c0153b3fdacb208a09a27421775c4
maojj <maojj@YSC-MAOJJ.kingser.com>
2017-10-20 16:02:17 +0800

    V2.6.3修改

--- okdeer-mall-order/src/main/java/com/okdeer/mall/order/builder/TradeOrderBuilder.java
+++ okdeer-mall-order/src/main/java/com/okdeer/mall/order/builder/TradeOrderBuilder.java
@@ -4,7 +4,6 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
