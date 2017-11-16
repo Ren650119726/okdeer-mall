5a087d18d98e5238b7b715890305edb4040a3dbf
hason <iluyasha@163.com>
2017-11-16 13:43:24 +0800

    V2.6.4修改

--- okdeer-mall-order/src/main/java/com/okdeer/mall/order/handler/impl/CheckFavourServiceImpl.java
+++ okdeer-mall-order/src/main/java/com/okdeer/mall/order/handler/impl/CheckFavourServiceImpl.java
@@ -3,12 +3,10 @@ package com.okdeer.mall.order.handler.impl;
 import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import javax.annotation.Resource;
 
 import org.apache.commons.lang3.StringUtils;
-import org.apache.shiro.util.CollectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
